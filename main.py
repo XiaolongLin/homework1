70ea45900d40157378b6bca5350bdf37cabfd7b2
Xiaolong Lin <xxl5453@psu.edu>
2020-09-06 03:58:43 +0000

    01

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 grade1=int[input("Enter your course 1 letter grade: ")]
 credit1=int[input("Enter your course 1 credit: ")]
-if(grade1=A):
-  gradepoint1=4.0
-elif(grade1=A-):
-  gradepoint=3.67
-elif(grade1=B+):
-  gradepoint1=3.33
-elif(grade1=B):
-  gradepoint1=3.0
-elif(grade1=B-):
-  gradepoint1=2.67
-elif(grade1=C+):
-  gradepoint1=2.33
-elif(grade1=C):
-  gradepoint1=2.0
-elif(grade1=D):
-  gradepoint1=1.0
-else
-  gradepoint1=0.0      
-
 print("Grade point for course 1 is:gradepoint1")
 grade2=int[input("Enter your course 2 letter grade: ")]
 credit2=int[input("Enter your course 2 credit: ")]
